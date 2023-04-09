4ba1ac70713934eead5ed31b3fa1b0d97df2b5e3
Thai Dang Tran <dtranthai1989@gmail.com>
2023-04-09 09:23:06 +0700

    init project

--- FashionWeb.Admin/Models/ErrorViewModel.cs
+++ FashionWeb.Admin/Models/ErrorViewModel.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace FashionWeb.Admin.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
