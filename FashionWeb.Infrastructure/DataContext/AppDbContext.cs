ff635fb367c158d69caffb1f31478bec643fdd7f
Thai Dang Tran <dtranthai1989@gmail.com>
2023-04-09 16:16:07 +0700

    FT-411 code first ef

--- FashionWeb.Infrastructure/DataContext/AppDbContext.cs
+++ FashionWeb.Infrastructure/DataContext/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FashionWeb.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,18 @@ namespace FashionWeb.Infrastructure.DataContext
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<OrderDetail>().HasKey(key => new {key.OrderId, key.ProductId});
+        }
     }
 }
