2e87535594615bdde16066b22c10bde07fed5328
Thai Dang Tran <dtranthai1989@gmail.com>
2023-05-13 21:44:42 +0700

    FT-450 call api get list product

---
+++ FashionWeb.Admin/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using FashionWeb.Domain.Services;
+using FashionWeb.Domain.ViewModels;
+using FashionWeb.Utilities.GlobalHelpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FashionWeb.Admin.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private readonly IProductService _productService;
+		private readonly ICategoryService _categoryService;
+		public ProductsController(IProductService productService,
+								 ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+			_productService = productService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var productViewModel = await _productService.GetProductViewModelAsync();
+			return View(productViewModel);
+		}
+	}
+}
