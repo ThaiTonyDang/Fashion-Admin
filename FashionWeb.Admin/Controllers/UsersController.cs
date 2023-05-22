4283acb43ed2b26ccfd82ebd7f471a64767b2e48
Tommy Dang <dtranthuy1995@gmail.com>
2023-05-22 22:21:21 +0700

    FT-471: Add login page

---
+++ FashionWeb.Admin/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FashionWeb.Admin.Controllers
+{
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+    }
+}
