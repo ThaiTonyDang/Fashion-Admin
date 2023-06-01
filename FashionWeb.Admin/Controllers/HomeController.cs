337da198309c2f45fbccf846e169e8b2e29dc636
Thai Dang Tran <dtranthai1989@gmail.com>
2023-06-01 11:29:42 +0700

    fix login error

--- FashionWeb.Admin/Controllers/HomeController.cs
+++ FashionWeb.Admin/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace FashionWeb.Admin.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
